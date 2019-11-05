759f63c1d259089d18cf0ea82a3feedd3e0b094d
WYZ0528 <1362268086@QQ.com>
2019-11-05 09:57:20 +0800

    week9的课上实验

--- code/test2.c
+++ code/test2.c
@@ -1,21 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
-int a;
-
-static int b=10;
-
-int main(int argc,char* argv[],char* envp[]){
-
-    int a=10;//stack
-
-    char* p=(char*)malloc(sizeof(char));//heap
-
-    printf("a=%d,p=0x%x\n",a,p);//动态链接库，*.so
-
-    getchar();//设置阻塞，也可以使用sleep()函数来解决
-
-    getchar();
-
+#include<unistd.h>
+#include<sys/types.h>
+static void __attribute__((constructor)) before_main()
+{
+    printf("Before Test Main!\n");
+}
+static void callback1(void)
+{
+    printf("callback1 is running!\n");
+}
+static void callback2(void)
+{
+    printf("callback2 is running!\n");
+}
+static void callback3(void)
+{
+    printf("callback3 is running!\n");
+}
+int main(int argc,char *argv[])
+{
+    int i;
+    atexit(callback1);
+    atexit(callback2);
+    atexit(callback3);
+    printf("test2:pid = %d,ppid = %d\n",getpid(),getppid());
+    for(i=0;i<argc;i++)
+        printf("test2: %d : %s\n",i,argv[i]);
+    printf("test2 Main function finished!\n");
+    //sleep(10);
     return 0;
-
 }
