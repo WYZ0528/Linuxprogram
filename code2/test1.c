83509b0caf691c232907eddb93779496ada06c4f
WYZ0528 <1362268086@QQ.com>
2019-11-06 16:33:35 +0800

    week9第二次课的实验代码

---
+++ code2/test1.c
@@ -0,0 +1,14 @@
+#include "my.h"
+static void __attribute__((constructor)) before_main()
+{
+    printf("----------Before Main!----------\n");
+}
+static void __attribute__((constructor)) after_main()
+{
+    printf("----------After Main!----------\n");
+}
+int main()
+{
+    printf("Main!\n");
+    return 0;
+}
