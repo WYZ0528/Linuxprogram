5415cce05fb24cb770c8680946d7ee22fe466790
WYZ0528 <1362268086@QQ.com>
2019-11-19 13:41:13 +0800

    第十周课堂测试

---
+++ code/2/2-1.c
@@ -0,0 +1,42 @@
+#include "my.h"
+
+static void callback1()
+{
+    printf("----------callback1----------\n");
+}
+static void callback2()
+{
+    printf("----------callback2----------\n");
+}
+static void __attribute__ ((constructor)) before_main(void)
+{
+    printf("----------before main----------\n");
+}
+static void __attribute__ ((constructor)) after_main(void)
+{
+    printf("----------after main----------\n");
+}
+
+int main(void)
+{
+    atexit(callback1);
+    atexit(callback2);
+    FILE *fp;
+    char buf[]={"fputs use full buffer"};
+    if((fp=fopen("test.dat","w+"))==NULL)
+    {
+	perror("failed to open!\n");
+	return -1;
+    }
+    if(fputs(buf,fp)==EOF)
+    {
+	perror("failed to fputs file!\n");
+	return -1;
+    }
+    printf("----------running in main!----------");
+    printf("printf use line buffer");
+    //while(1);
+    //exit(0);
+    _exit(0);
+    //return 0;
+}
---
+++ code/2/my.h
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+#include<math.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<string.h>
