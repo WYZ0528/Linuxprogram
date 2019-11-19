ed956230fc8bbe2e45c9c68648a23faac2e582c4
WYZ0528 <1362268086@QQ.com>
2019-11-19 14:31:31 +0800

    第十一周的课堂实验

--- code/2/my.h
+++
@@ -1,9 +0,0 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<time.h>
-#include<math.h>
-#include<unistd.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<fcntl.h>
-#include<string.h>
---
+++ code/my.h
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<wait.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<sys/types.h>
