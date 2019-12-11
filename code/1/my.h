061dac420bb4931f5e31024bc6e7f9a5c78f385b
WYZ0528 <1362268086@QQ.com>
2019-12-11 16:09:08 +0800

    第十四周第一次课

---
+++ code/1/my.h
@@ -0,0 +1,10 @@
+#include<pthread.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<limits.h>
+#include<string.h>
+#include<sys/time.h>
+#include<syscall.h>
+#define NUM 4
+int count(int);
